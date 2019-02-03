c9ea1927aebc5ed2d84136a2cd10e2ac8c79e0f8
tanzy <tan.zy096@gmail.com>
2019-02-03 16:15:31 +0800

    Separate functions and main body

--- CGPAcalc/CGPAcalculator.py
+++ CGPAcalc/CGPAcalculator.py
@@ -1,25 +1,27 @@
 import cgpafunctions
 import cgpafunctionsTemplate
 
+
 def main():
-        print("""
+    print("""
     -----Welcome to CGPA calculator!------\n
     1. CGPA Calculator
     2. GPA calculator
     3. Exit
         """)
-        choice = int(input())
+    choice = int(input())
 
-        if choice == 1:
-            CGPA_calc()
-        elif choice == 2:
-            GPA_calc()
-        elif choice == 3:
-            print("Thanks for using this calculator!")
-            exit()
-        else:
-            print("Please enter a valid option.")
+    if choice == 1:
+        CGPA_calc()
+    elif choice == 2:
+        GPA_calc()
+    elif choice == 3:
+        print("Thanks for using this calculator!")
+        exit()
+    else:
+        print("Please enter a valid option.")
+
 
 while __name__ == "__main__":
-    #intro
+    # intro
     main()
--- CGPAcalc/README.md
+++ CGPAcalc/README.md
@@ -1,9 +1,11 @@
 # CGPA Calculator #
 
 ### Instructions ###
-1) For those who wish to work on the code on their own first, access `CGPAcalculatorTemplate.py`.
+The main function file is `CGPAcalculator.py`.
 
-2) For those who wish to view my original code, access `CGPAcalculator.py`.
+1) For those who wish to work on the code on their own first, access `cgpafunctionsTemplate.py`.
+
+2) For those who wish to view my original code, access `cgpafunctions.py`.
 
 3) For those who simply want to see how my code looks, visit [Trinket.io](https://trinket.io/python3/d3078fc168?outputOnly=true&runOption=run&showInstructions=true).
 
@@ -12,4 +14,3 @@ P.S. When I mention AU for GPA computation, it refers to the number of graded AU
 
 If you have any queries or bugs to raise, please leave a comment!<br>
 Cheers!
-
